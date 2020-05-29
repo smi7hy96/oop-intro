64a7348451f8871e0a58d767c54b48eddf33f5bd
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-29 15:34:09 +0100

    added tests for cat_class

--- cat_class_tests.py
+++ cat_class_tests.py
@@ -4,7 +4,25 @@ from cat_class import Cat
 
 class CatTest(unittest.TestCase):
     def SetUp(self):
-        self.cat = Cat()
+        self.cat = Cat('Aggressive', 'Ginger', 'Mufasa', False)
+
+    def test_eat(self):
+        self.assertEqual(self.cat.eat('tuna'), 'ugh, tuna')
+
+    def test_sleep(self):
+        self.assertEqual((self.cat.sleep()), '*snore*')
+
+    def test_attack(self):
+        self.assertEqual(self.cat.attack(), 'HISS')
+        self.assertEqual(self.cat.attack(), '*snore*')
+
+    def test_chase(self):
+        self.assertEqual(self.cat.chase('mouse'), 'Come here Jerry')
+        self.assertEqual(self.cat.chase('rabbit'), 'Not interested')
+
+    def test_play(self):
+        self.assertEqual(self.cat.play('box'), 'If I fits....')
+        self.assertEqual(self.cat.play('new toy'), "Erm no. Where's the box?")
 
 
 if __name__ == '__main__':
