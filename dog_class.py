e87bc74ee93de6f82b1465f2e791adac047e484a
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-29 15:18:26 +0100

    finished tests for dog class and made them pass

--- dog_class.py
+++ dog_class.py
@@ -3,8 +3,24 @@
 
 class Dog:
 
-    def __init__(self, name):
+    def __init__(self, name, breed, size):
         self.name = name
+        self.breed = breed
+        self.size = size
 
     def bark(self):
-        return f'{self.name}, bark'
+        return f'{self.size} bark'
+
+    def eat(self, food):
+        return 'oooh, ' + food.lower()
+
+    def sleep(self):
+        return '*snore*'
+
+    def fetch(self, item):
+        if item == "ball":
+            return "CHAAAASE"
+        elif item == "stick":
+            return "GET THA STICK"
+        else:
+            return "*yawn* i'll sit back down"
--- dog_class_tests.py
+++ dog_class_tests.py
@@ -1,11 +1,24 @@
-from unittest import TestCase
+import unittest
 from dog_class import Dog
 
 
-class DogTest(TestCase):
+class DogTest(unittest.TestCase):
     def setUp(self):
-        self.dog = Dog('Scooby')
+        self.dog = Dog('Scooby', 'Great Dane', 'BIG')
 
     def test_bark(self):
+        self.assertEqual(self.dog.bark(), f'{self.dog.size} bark')
 
-        self.assertEqual(self.dog.bark(), f'{self.dog.name}, bark')
+    def test_eat(self):
+        self.assertEqual(self.dog.eat('Scooby Snax'), 'oooh, scooby snax')
+
+    def test_sleep(self):
+        self.assertEqual((self.dog.sleep()), '*snore*')
+
+    def test_fetch(self):
+        self.assertEqual(self.dog.fetch('ball'), 'CHAAAASE')
+        self.assertEqual(self.dog.fetch('stick'), 'GET THA STICK')
+        self.assertEqual(self.dog.fetch('leaf'), "*yawn* i'll sit back down")
+
+if __name__ == '__main__':
+    unittest.main()
