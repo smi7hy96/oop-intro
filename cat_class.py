b923b8300b0140d3f8723e57807cc10466e14e5f
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-29 15:53:25 +0100

    added chase method, passed test

--- cat_class.py
+++ cat_class.py
@@ -12,12 +12,21 @@ class Cat:
 
     def sleep(self):
         if self.sleepy:
+            self.sleepy = True
             return '*snore*'
         else:
             return 'I wanna play!'
 
     def attack(self):
         if not self.sleepy:
+            self.sleepy = True
             return 'HISS'
         else:
             return self.sleep()
+
+    def chase(self, animal):
+        if animal == 'mouse':
+            self.sleepy = True
+            return 'Come here Jerry'
+        else:
+            return 'Not interested'
